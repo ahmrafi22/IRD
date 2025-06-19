185a65af480fbf437249a305b676417b1df7e85f
Rafi <163875268+ahmrafi22@users.noreply.github.com>
2025-06-19 21:55:59 +0600

    for render lib update

--- lib/db.ts
+++ lib/db.ts
@@ -1,8 +1,21 @@
+
 import Database from "better-sqlite3";
 import path from "path";
+import fs from "fs"; 
+
+const PERSISTENT_DISK_MOUNT_PATH = process.env.SQLITE_DB_DIR || "/var/data"; 
 
 
-const dbPath = path.resolve(process.cwd(), "dua_main.sqlite");
+const dbDirectory = path.resolve(PERSISTENT_DISK_MOUNT_PATH);
+if (!fs.existsSync(dbDirectory)) {
+  fs.mkdirSync(dbDirectory, { recursive: true });
+}
 
-const db = new Database(dbPath); 
+const dbPath = path.join(dbDirectory, "dua_main.sqlite");
+
+console.log(`SQLite database path: ${dbPath}`); 
+
+const db = new Database(dbPath);
+
+
 export default db;
